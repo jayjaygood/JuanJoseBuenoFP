f0093b2a1c0ff0af3325e95ae6faf7962a68dc57
Juan José Bueno <buenoboteroj@gmail.com>
2025-08-01 15:23:58 -0500

    Variables, constantes y tipos de datos

--- 1.InicioVisualStudio/Program.cs
+++ 1.InicioVisualStudio/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Esta es la declaracion e inicializacion de una variable
+            string nombre="Alexander Hamilton";
+
+            Console.Write("Hola tonoto, " + nombre + "\n");
+            Console.Write("Fiumba\n");
+
+            //Tipos de datos
+            byte dato1 = 255;
+            int dato2 = 202321;
+            long dato3 = 9202125235;
+            double dato4 = 326.625;
+            char dato5 = '&'; //puede almacenar un solo caracter
+            bool dato6 = false;//Almacena true or false 
+            object dato7 = new Program();
+
         }
     }
 }
