af2986c043669e434bd0c376b8be54aa6ac27d6b
Juan José Bueno <buenoboteroj@gmail.com>
2025-08-29 15:33:05 -0500

    Add files via upload
    9.Ciclo While

---
+++ 9.B.CicloWhile/9.B.CicloWhile/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _9.B.CicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crear un algoritmo que permita calcular la factorial de un número dado por el usuario
+             (5! = 1*2*3*4*5)*/
+
+            int num = 0;
+            int acumulador = 1;
+            int contador = 1;
+
+            Console.WriteLine("Ingrese un número para calcular su factorial");
+            num=int.Parse(Console.ReadLine());
+
+            while (contador <= num) 
+            {
+                acumulador=acumulador*contador;
+                Console.WriteLine($"Contador: {contador} - FaSctorial: {acumulador}");
+                contador++;
+            }
+            Console.WriteLine($"El factorial del número {num}, es {acumulador}");
+        }
+    }
+}
---
+++ 9.CicloWhite/9.CicloWhite/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _9.CicloWhite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escribir un algoritmo que permita generar primeros 5 
+             * números enteros positivos y realizar y mostrar su suma*/
+
+            int contador = 0;
+            int acumulador = 0;
+
+            while (contador <= 5) 
+            {
+                acumulador += contador; //acumulador=acumulador+contador
+                Console.WriteLine($"C: {contador} - A: {acumulador}");
+                contador++;
+            }
+
+            Console.WriteLine($"La suma de los cinco primeros números enteros es: {acumulador}");
+        }
+    }
+}
